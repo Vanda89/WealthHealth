70ea5fb0f40f1bb161161d4384d6a6199d046220
Vanda89 <sandrinealcazar@gmail.com>
2024-06-13 19:03:04 +0200

    feat :Added all necessary dependencies to the project for routing, state management, style and code quality

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/no-unresolved': 'error',
+    'prettier/prettier': [
+      'error',
+      { endOfLine: 'auto', singleQuote: true, semi: false },
+    ],
+  },
+}
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Router from './Router.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter></BrowserRouter>
+    </React.StrictMode>
+  )
+}
+
+export default App
