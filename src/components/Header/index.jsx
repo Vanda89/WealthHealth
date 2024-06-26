d8192f43dd15775f71818189ad4e093b8bced253
Vanda89 <sandrinealcazar@gmail.com>
2024-06-26 08:29:47 +0200

    feat: Add Header and logo to the project

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,42 @@
+import logo from '../../assets/logo/logo_wealth_health.png'
+import { NavLink } from 'react-router-dom'
+import { ROUTES } from '../../config.js'
+
 export default function Header() {
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <h1 className="text-2xl font-bold">WealthHealth</h1>
+    <header className="flex items-center justify-between py-2 pl-5 pr-10 border-custom-gray-200 bg-white  border-b-3">
+      <div className="logo-container w-30 ">
+        <img
+          src={logo}
+          alt="Logo de l'entreprise Wealth Health"
+          className="h-24"
+        />
+      </div>
+      <nav className=" w-70">
+        <ul className="flex justify-end gap-10">
+          <NavLink
+            to={ROUTES.HOME}
+            className={({ isActive }) =>
+              isActive
+                ? 'text-custom-green-300 underline underline-offset-2 decoration-custom-green-300'
+                : 'text-custom-gray-900 hover:text-custom-green-300'
+            }
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to={ROUTES.EMPLOYEE_LIST}
+            className={({ isActive }) =>
+              isActive
+                ? 'text-custom-green-300 underline underline-offset-2 decoration-custom-green-300'
+                : 'text-custom-gray-900 hover:text-custom-green-300'
+            }
+          >
+            Staff
+          </NavLink>
+        </ul>
+      </nav>
     </header>
   )
 }
