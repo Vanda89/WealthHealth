c57ff7e8594d467ecce0d6d3fe2a7ec06db6fc80
Vanda89 <sandrinealcazar@gmail.com>
2024-07-17 08:36:20 +0200

    feat: Add Home component with registration form logic, integrate my modal-reactjs-tailwindcss plugin, and handle form submission and modal closing

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,34 @@
 import RegisterForm from '../../components/RegisterForm'
+import Modal from '../../../node_modules/modal-reactjs-tailwindcss'
+import { data } from '../../utils/dropdownData'
+import { useState } from 'react'
+import { FaCheck } from 'react-icons/fa'
+import { FaXmark } from 'react-icons/fa6'
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false)
+  const handleFormSubmit = () => {
+    setIsOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsOpen(false)
+  }
+
   return (
-    <main className=" flex flex-col items-center justify-center gap-6 py-44">
+    <main className=" flex flex-col items-center justify-center gap-14 py-44">
       <h1 className="text-6xl text-custom-green-300">HRnet</h1>
-      <RegisterForm />
+      <RegisterForm dropdownData={data} onFormSubmit={handleFormSubmit} />
+      <Modal
+        isOpen={isOpen}
+        isHidden={true}
+        onClose={handleCloseModal}
+        closeIcon={FaXmark}
+        infoIcon={FaCheck}
+        infoContainerColor="bg-green-100"
+        infoIconColor="text-green-500"
+        infoIconSize="text-5xl"
+      />
     </main>
   )
 }
