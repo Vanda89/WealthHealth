92608ea38ae68604e006b1e817fb3859c893c83a
Vanda89 <sandrinealcazar@gmail.com>
2024-10-17 14:08:59 +0200

    feat(header): add link to logo and update header styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import logo from '/assets/logo/logo_wealth_health.png'
+import logo from '/assets/logo/logo_wealth_health.webp'
 import { NavLink } from 'react-router-dom'
 import { ROUTES } from '../../config.js'
 
@@ -6,35 +6,49 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between py-2 pl-5 pr-10 border-custom-gray-200 bg-white  border-b-3">
       <div className="logo-container w-30 ">
-        <img
-          src={logo}
-          alt="Logo de l'entreprise Wealth Health"
-          className="h-24"
-        />
+        <NavLink
+          to={ROUTES.HOME}
+          className={({ isActive }) =>
+            isActive
+              ? 'font-medium text-custom-green-700 underline underline-offset-2 decoration-custom-green-300'
+              : 'text-custom-gray-900 hover:text-custom-green-700'
+          }
+        >
+          <img
+            src={logo}
+            alt="Logo de l'entreprise Wealth Health"
+            className="h-24"
+            width="90"
+            height="96"
+          />
+        </NavLink>
       </div>
       <nav className=" w-70">
         <ul className="flex justify-end gap-10 text-lg">
-          <NavLink
-            to={ROUTES.HOME}
-            className={({ isActive }) =>
-              isActive
-                ? 'font-medium text-custom-green-300 underline underline-offset-2 decoration-custom-green-300'
-                : 'text-custom-gray-900 hover:text-custom-green-300'
-            }
-          >
-            Home
-          </NavLink>
-
-          <NavLink
-            to={ROUTES.EMPLOYEE_LIST}
-            className={({ isActive }) =>
-              isActive
-                ? 'font-medium text-custom-green-300 underline underline-offset-2 decoration-custom-green-300'
-                : 'text-custom-gray-900 hover:text-custom-green-300'
-            }
-          >
-            Staff
-          </NavLink>
+          <li>
+            <NavLink
+              to={ROUTES.HOME}
+              className={({ isActive }) =>
+                isActive
+                  ? 'font-medium text-custom-green-700 underline underline-offset-2 decoration-custom-green-300'
+                  : 'text-custom-gray-900 hover:text-custom-green-700'
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={ROUTES.EMPLOYEE_LIST}
+              className={({ isActive }) =>
+                isActive
+                  ? 'font-medium text-custom-green-700 underline underline-offset-2 decoration-custom-green-300'
+                  : 'text-custom-gray-900 hover:text-custom-green-700'
+              }
+            >
+              Staff
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </header>
